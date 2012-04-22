785bb2d83cd7d3f65d292b7d121bb95fb61320c0
arpruss <arpruss@d58f9c0b-7949-19ce-40d7-806c7eefa3ec>
2012-04-22 03:25:00 +0000

    foo

--- trunk/src/mobi/omegacentauri/PerApp/OrientationSetting.java
+++ trunk/src/mobi/omegacentauri/PerApp/OrientationSetting.java
@@ -1,6 +1,18 @@
 package mobi.omegacentauri.PerApp;
 
+import mobi.omegacentauri.PerApp.R;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class OrientationSetting extends Setting {
 	public OrientationSetting(SharedPreferences pref) {
@@ -8,5 +20,30 @@ public class OrientationSetting extends Setting {
 		
 		name = "Orientation";
 		id = "orientationSetting";
+		defaultValue = "0";
+	}
+	
+	public void dialog(Activity activity, final String app) {
+		load(app);
+		
+		View v = getDialogView(activity, R.layout.orientation_setting);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+		builder.setView(v);
+		
+		CheckBox cb = (CheckBox)v.findViewById(R.id.lock);
+			
+		cb.setChecked(intValue != 0);
+		
+		cb.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+
+			@Override
+			public void onCheckedChanged(CompoundButton button, boolean value) {
+				OrientationSetting.this.intValue = value ? 1 : 0;
+				save(app);
+			}
+		});
+		
+		builder.create().show();
 	}
 }
